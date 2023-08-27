bad8d6294bb71524d0f31d61dbb9237281b227f7
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-08-28 00:15:10 +0200

    Update auth.py

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,20 +1,20 @@
-from fastapi import APIRouter, HTTPException, Depends, status, Security
+from fastapi import APIRouter, HTTPException, Depends, status, Security, BackgroundTasks, Request
 from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials, HTTPBearer
+from fastapi_mail import MessageSchema, FastMail, MessageType
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
 from src.repository import users as repository_users
-from src.schemas import UserModel, UserResponse, TokenModel
+from src.schemas import UserModel, UserResponse, TokenModel, RequestEmail, EmailSchema
 from src.services.auth import auth_service
+from src.services.email_service import send_email_confirmation, conf
 
 router = APIRouter(prefix='/auth', tags=["auth"])
 security = HTTPBearer()
 
 
 @router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def signup(
-        body: UserModel,
-        db: Session = Depends(get_db)
+async def signup(body: UserModel, background_tasks: BackgroundTasks, request: Request, db: Session = Depends(get_db)
 ) -> dict:
     """Обробляє операцію POST. Вона створює нового користувача, якщо користувача з такою електронною поштою не існує.
      Не може бути в системі два користувача з однаковим email. Якщо користувач з таким email вже існує в базі даних,
@@ -23,11 +23,11 @@ async def signup(
     exist_user = await repository_users.get_user_by_email(body.email, db)
     if exist_user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account already exists")
-
+    print(request.base_url)
     body.password = auth_service.get_password_hash(body.password)
     new_user = await repository_users.create_user(body, db)
-
-    return {"user": new_user, "detail": "User successfully created"}
+    background_tasks.add_task(send_email_confirmation, new_user.email, new_user.username, request.base_url)
+    return {"user": new_user, "detail": "User successfully created. Check your email for confirmation."}
 
 
 @router.post("/login", response_model=TokenModel)
@@ -44,7 +44,8 @@ async def login(
     user = await repository_users.get_user_by_email(body.username, db)
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email")
-
+    if not user.confirmed:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Email not confirmed")
     if not auth_service.verify_password(body.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password")
     # Generate JWT
@@ -76,3 +77,40 @@ async def refresh_token(
     await repository_users.update_token(user, refresh_token, db)
 
     return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+
+@router.post("/send-email")
+async def send_in_background(background_tasks: BackgroundTasks, body: EmailSchema):
+    message = MessageSchema(
+        subject="Account confirmation",
+        recipients=[body.email],
+        template_body={"fullname": "Billy Jones"},
+        subtype=MessageType.html
+    )
+    fm = FastMail(conf)
+    background_tasks.add_task(fm.send_message, message, template_name="example_email.html")
+    return {"message": "email has been sent"}
+
+
+@router.get('/confirmed_email/{token}')
+async def confirmed_email(token: str, db: Session = Depends(get_db)):
+    email = await auth_service.get_email_from_token(token)
+    user = await repository_users.get_user_by_email(email, db)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Verification error")
+    if user.confirmed:
+        return {"message": "Your email is already confirmed"}
+    await repository_users.confirmed_email(email, db)
+    return {"message": "Email confirmed"}
+
+
+@router.post('/email-confirmation-request')
+async def request_email(body: RequestEmail, background_tasks: BackgroundTasks, request: Request,
+                        db: Session = Depends(get_db)):
+    user = await repository_users.get_user_by_email(body.email, db)
+    print(request.base_url)
+    if user.confirmed:
+        return {"message": "Your email is already confirmed"}
+    if user:
+        background_tasks.add_task(send_email_confirmation, user.email, user.username, request.base_url)
+    return {"message": "Check your email for confirmation."}
