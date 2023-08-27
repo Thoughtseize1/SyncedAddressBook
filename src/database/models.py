ba7ab11384000b15eb2df445c1407fb355d2b1b9
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-08-28 00:14:37 +0200

    Update my models

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, DateTime, func, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, DateTime, func, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -30,6 +30,7 @@ class User(Base):
     created_at = Column('crated_at', DateTime, default=func.now())
     avatar = Column(String(255), nullable=True)
     refresh_token = Column(String(255), nullable=True)
+    confirmed = Column(Boolean, default=False)
 
 
 
