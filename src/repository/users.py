498b2f5ebe3f5342a79431e456ce6a2ace6e5707
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-08-28 00:15:03 +0200

    Added func for confirming email

--- src/repository/users.py
+++ src/repository/users.py
@@ -33,3 +33,9 @@ async def update_token(user: User, token: str | None, db: Session) -> None:
     Вона оновлює поле refresh_token користувача та фіксує зміни у базі даних."""
     user.refresh_token = token
     db.commit()
+
+
+async def confirmed_email(email: str, db: Session) -> None:
+    user = await get_user_by_email(email, db)
+    user.confirmed = True
+    db.commit()
